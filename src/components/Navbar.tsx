80ef28dff25c50ffda78f7fe8e79b2e03e602e14
ChiKien753 <duongchikien2001@gmail.com>
2023-05-02 07:54:25 +0700

    1st iniit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { HiMenuAlt4 } from "react-icons/hi"
+import { AiOutlineClose } from "react-icons/ai"
+
+import logo from "../assets/images/logo.png"
+export default function Navbar() {
+    return (
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial"></ul>
+        </nav>
+    )
+}
